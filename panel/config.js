fec392042ce642be1713da7abbc673511fbb31a0
M4G4 <124450800+Santitoon@users.noreply.github.com>
2026-02-22 16:53:24 -0500

    Update config.js

--- panel/config.js
+++ panel/config.js
@@ -1,5 +1,7 @@
-// NEXCARD Panel Config
-window.NEXCARD_CONFIG = {
-  API_BASE: "https://script.google.com/macros/s/AKfycbzPNPQLXoDhlfekyAmsbnQIIO_pkzPdF8hNy7WKjBohgvDZmEToOyYzW1J_YIRVmcKgXw/exec",
-  TOKEN_KEY: "nexcard_token_v1"
+// config.js
+window.NEXCARD = {
+  API_BASE: "https://script.google.com/macros/s/AKfycbx7ghGSC47jyKY5ArI2s1JpO3UILEHVzmfaHxYxG3sLYfN3QDyFYWnXFStRLHvdJGtXRQ/exec",
+  TOKEN_KEY: "nexcard_token",
+  EMPRESA_KEY: "nexcard_empresa_id",
+  ALIAS_KEY: "nexcard_empresa_alias"
 };
